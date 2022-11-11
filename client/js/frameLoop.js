4b9f1033331ae2055982bb7bd4bc535ca54ca443
Electruuu <60323922+Electruuu@users.noreply.github.com>
2022-11-11 23:34:36 +0100

    delta time (maybe)

--- client/js/frameLoop.js
+++ client/js/frameLoop.js
@@ -1,9 +1,12 @@
-//let 
+let previousTimeStamp
 
-export default function frameLoop() {
+export default function frameLoop(timestamp) {
+    const delta = timestamp - previousTimeStamp;
+
     window.ctx.clearRect(0, 0, window.canvas.width, window.canvas.height)
 
-    document.dispatchEvent(new CustomEvent('drawTick'))
-
+    document.dispatchEvent(new CustomEvent('drawTick', {delta: delta}))
+    
+    previousTimeStamp = timestamp
     window.requestAnimationFrame(frameLoop)
 }
